85aa2b797575c7c0c0ed9aa26625b22f6ad092fc
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2023-11-23 00:10:34 -0600

    Button test (bug?)

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        document.getElementById('navList').style.display = 'none';
+    }
+};
+
 document.getElementById('dropdownButton').addEventListener('click', function() {
     var navList = document.getElementById('navList');
     if (navList.style.display === 'none') {
