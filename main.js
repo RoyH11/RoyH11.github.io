151b803f54c5c766cd7c4ae2b16e5cb17ff2280c
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2024-02-07 19:02:26 -0600

    fixed dropdown button bug

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 window.onload = function() {
-    if (window.matchMedia("(max-width: 600px)").matches) {
-        document.getElementById('navList').style.display = 'none';
-    }
-};
-
-document.getElementById('dropdownButton').addEventListener('click', function() {
+    var dropdownButton = document.getElementById('dropdownButton');
     var navList = document.getElementById('navList');
-    if (navList.style.display === 'none') {
-        navList.style.display = 'block';
-    } else {
-        navList.style.display = 'none';
-    }
-});
+    navList.style.display = 'none';
+
+    dropdownButton.addEventListener('click', function() {
+        var display = navList.style.display;
+        
+        if (display == 'none') {
+            navList.style.display = 'block';
+        } else {
+            navList.style.display = 'none';
+        }
+    });
+};
